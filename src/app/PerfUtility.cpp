a68c4633ab7886d0ca5f6e7092eb9a33784e46b9
Jens Edhammer <tobyndax@hotmail.com>
2017-07-26 14:29:58 +0200

    Added performance utility class

---
+++ src/app/PerfUtility.cpp
@@ -0,0 +1,46 @@
+#include "PerfUtility.h"
+#include <chrono>
+#include <iostream>
+#include <numeric>
+PerfUtility::PerfUtility()
+{
+
+}
+PerfUtility::~PerfUtility()
+{
+
+}
+
+void PerfUtility::printTimeSumBetween(int startInd, int endInd)
+{
+	long long totalDuration;
+	if (endInd == 0) {
+		totalDuration = std::accumulate(durations.begin() + startInd, durations.end(), 0.0);
+	}
+	else {
+		totalDuration = std::accumulate(durations.begin() + startInd, durations.begin() + endInd, 0.0);
+	}
+	std::cout << "Sub- execution time: ";
+	std::cout << totalDuration / 1000.0f << "ms" << std::endl;
+}
+void PerfUtility::printTotalTime() {
+	long long totalDuration = std::accumulate(durations.begin(), durations.end(), 0.0);
+	std::cout << "Total execution time: ";
+	std::cout <<  totalDuration / 1000.0f << "ms" << std::endl;
+}
+
+void PerfUtility::measureFunction(std::function<void (void)> func, std::string functionName)
+{
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+	func();
+
+	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+	durations.push_back(duration);
+
+	std::cout << "Execution of function " + functionName + " took: ";
+
+	std::cout << duration / 1000.0f << "ms" << std::endl;
+}
---
+++ src/app/PerfUtility.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <functional>
+#include <chrono>
+#include <vector>
+
+class PerfUtility {
+public:
+	PerfUtility();
+	~PerfUtility();
+	
+	void printTotalTime();
+	void measureFunction(std::function<void (void)>, std::string functionName);
+	void printTimeSumBetween(int startInd, int endInd = 0);
+private: 
+	std::vector<long long> durations = std::vector <long long>();
+};
